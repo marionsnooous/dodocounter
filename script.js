211976be1ec5661331cff37886f10f427504bbe7
Thomas Duplomb <thomas.duplomb@gmail.com>
2013-11-23 00:14:32 -0800

    add comments to explain the code

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 var dodoCounter = function () {
+  // C'est ici que tu vas ecrire le code (Toute les lignes qui commencent par // sont des commentaire
+  // et sont ignoree par l'ordinateur)
   return "Il est reste plusieurs dodos avant que Tom arrive";
 };
 
+// Pour l'instant, c'est pas la peine que tu fasse attention a ce qui suit. Je
+// t'expliquerais plus tard.
 var runner = function () {
   $("#container").html(dodoCounter());
   window.setTimeout(runner, 1000);
